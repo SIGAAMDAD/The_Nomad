943585ddc83dde71987742e953190525fa53a705
Noah Van Til <86020846+SIGAAMDAD@users.noreply.github.com>
2025-02-07 11:44:34 -0600

    Update team_brawl.gd

--- scripts/multiplayer/modes/team_brawl.gd
+++ scripts/multiplayer/modes/team_brawl.gd
@@ -1,26 +1,32 @@
 class_name TeamBrawlMode extends MultiplayerMode
 
-var _team_1_score:int = 0
-var _team_2_score:int = 0
+var _team1:Team = null
+var _team2:Team = null
+var _killfeed:KillFeed = null
 
-var _team_1_players:Array[ Player ]
-var _team_2_players:Array[ Player ]
-
-func _on_player_death( attacker: EntityBase, target: EntityBase ) -> void:
-	if attacker != null and attacker is not Player:
-		push_error( "_on_player_death(team_brawl): attacker is not a player object!" )
+func _on_player_death( attacker, EntityBase, target: EntityBase ) -> void:
+	if attacker != null && attacker is not Player:
+		push_error( "_on_player_death(team_brawl) attacker is not a player object!" )
 		return
 	if target is not Player:
 		push_error( "_on_player_death(team_brawl): target is not a player object!" )
 		return
 	
+	# we didn't kill ourselves...
 	if attacker != null:
-		match attacker._multiplayer_team:
-			1:
-				_team_1_score += 1
-			2:
-				_team_2_score += 1
+		attacker._multiplayer_team._score += 1
+		attacker._multiplayer_kills += 1
+	
+	target._multiplayer_deaths += 1
+	_killfeed.push( attacker, target )
 
-func add_player_to_team1( player: Player ) -> void:
-	_team_1_players.push_back( player )
-	player.on_player_death.connect( _on_player_death )
+func init( team1: Team, team2: Team, killfeed: KillFeed ) -> void:
+	_team1 = team1
+	_team2 = team2
+	_killfeed = killfeed
+	
+	for player in _team1:
+		player.on_player_death.connect( _on_player_death )
+	
+	for player in _team2:
+		player.on_player_death.connect( _on_player_death )
