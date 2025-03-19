442c6739b78835310b5888e5bd86ac17ff5e1640
Noah Van Til <86020846+SIGAAMDAD@users.noreply.github.com>
2025-03-19 12:26:06 -0500

    Update MultiplayerData.cs

--- scripts/multiplayer/MultiplayerData.cs
+++ scripts/multiplayer/MultiplayerData.cs
@@ -11,18 +11,6 @@ public partial class MultiplayerData : Node2D {
 
 	private PackedScene PlayerScene = null;
 
-	public enum UpdateType : uint {
-		WeaponSlots,
-		WeaponsStack,
-		AmmoLight,
-		AmmoHeavy,
-		AmmoPellets,
-		Consumables,
-		MiscItems,
-
-		Count
-	};
-
 	public class Team {
 		public List<Player> Players = new List<Player>();
 		public int Score = 0;
@@ -38,12 +26,16 @@ public partial class MultiplayerData : Node2D {
 	private Mode ModeData = null;
 	private Dictionary<ulong, Player> Players = null;
 	private Player ThisPlayer;
+	
+	private Node PlayerList = null;
+	
+	private Control PauseMenu = null;
 
 	// prebuild packets
 	private Dictionary<string, object> GameData = new Dictionary<string, object>();
 	private Dictionary<string, object> Packets = new Dictionary<string, object>();
 	private Dictionary<string, object> Update = new Dictionary<string, object>();
-
+	
 	public Mode.GameMode GetMode() {
 		return ModeData.GetMode();
 	}
@@ -52,8 +44,6 @@ public partial class MultiplayerData : Node2D {
 	}
 
 	public void ProcessHeartbeat( Dictionary<string, object>  data ) {
-		ModeData.SetMode( (Mode.GameMode)(int)data[ "mode" ] );
-
 		foreach ( var player in Players.Values ) {
 			Dictionary<string, object> values = (Dictionary<string, object>)data[ player.MultiplayerId.ToString() ];
 
@@ -72,31 +62,34 @@ public partial class MultiplayerData : Node2D {
 		}
 	}
 	public void ProcessClientData( ulong senderId, Dictionary<string, object> packet ) {
-		Player player = Players[ senderId ];
-
+		NetworkPlayer player = Players[ senderId ] as NetworkPlayer;
+		
 		player.SetHealth( (float)packet[ "health" ] );
 		player.SetRage( (float)packet[ "rage" ] );
 		player.SetFlags( (Player.PlayerFlags)packet[ "flags" ] );
 		player.SetHandsUsed( (Player.Hands)packet[ "hands_used" ] );
 		player.GlobalPosition = (Godot.Vector2)packet[ "position" ];
-		player.SetArmAngle( (float)packet[ "rotation" ] );
+		
+		PlayerSystem.Arm leftArm = player.GetArmLeft();
+		PlayerSystem.Arm rightArm = player.GetArmRight();
+		
+		leftArm.SetMode( (WeaponEntity.Property)packet[ "arm_left_mode" ] );
+		leftArm.SetWeapon( (int)packet[ "arm_left_slot" ] );
+		leftArm.GlobalRotation = (float)packet[ "arm_left_rotation" ];
+		
+		rightArm.SetMode( (WeaponEntity.Property)packet[ "arm_right_mode" ] );
+		rightArm.SetWeapon( (int)packet[ "arm_right_slot" ] );
+		rightArm.GlobalRotation = (float)packet[ "arm_right_rotation" ];
 	}
-	public void ProcessPlayerUpdate( ulong senderId, Dictionary<string, object> packet ) {
-
+	public void ProcessStatusUpdate( Dictionary<string, object> packet ) {
+		Player player = Players[ (ulong)packet[ "id" ] ];
+		player.SetHealth( (float)packet[ "health" ] );
+		player.SetFlags( (PlayerFlags)packet[ "flags" ] );
 	}
-
-	public void SendPlayerUpdate( Player player, UpdateType type ) {
-		switch ( type ) {
-		case UpdateType.WeaponsStack:
-			Update[ "data" ] = player.GetWeaponStack();
-			break;
-		case UpdateType.WeaponSlots:
-			Update[ "slots" ] = player.GetWeaponSlots();
-			break;
-		};
-
-	}
-
+	
+	/// <summary>
+	///	sends gamestate data from the host to all the clients
+	/// </summary>
 	private void OnServerHeartbeatTimeout() {
 		if ( !SteamLobby.Instance.IsOwner() ) {
 			return; // we're not the host
@@ -130,22 +123,60 @@ public partial class MultiplayerData : Node2D {
 		Packets[ "rage" ] = ThisPlayer.GetRage();
 		Packets[ "flags" ] = ThisPlayer.GetFlags();
 		Packets[ "hands_used" ] = ThisPlayer.GetHandsUsed();
-		Packets[ "rotation" ] = ThisPlayer.GetArmAngle();
 		Packets[ "position" ] = ThisPlayer.GlobalPosition;
-
+		Packets[ "weapon" ] = (string)ThisPlayer.GetWeaponSlots()[ ThisPlayer.GetCurrentWeapon() ].GetWeapon().Data.Get( "id" );
+		
+		PlayerSystem.Arm leftArm = ThisPlayer.GetArmLeft();
+		Packets[ "arm_left_rotation" ] = leftArm.GlobalRotation;
+		Packets[ "arm_left_mode" ] = leftArm.GetMode();
+		
+		PlayerSystem.Arm rightArm = ThisPlayer.GetArmRight();
+		Packets[ "arm_right_rotation" ] = rightArm.GlobalRotation;
+		Packets[ "arm_right_mode" ] = rightArm.GetMode();
+		
 		SteamLobby.Instance.SendP2PPacket( 0, Packets );
 	}
 
+	private void OnPlayerJoined( ulong steamId ) {
+		GetNode( "/root/Console" ).Call( "print_line", "Adding " + steamId + " to game..." );
+		if ( Players.ContainsKey( steamId ) ) {
+			return;
+		}
+		
+		Player player = PlayerScene.Instantiate<Player>();
+		player.MutliplayerUserName = Steam.GetFriendPersonaName( steamId );
+		player.MutliplayerId = steamId;
+		SpawnPlayer( player );
+		Players.Add( steamId, player );
+		PlayerList.AddChild( player );
+	}
+	private void OnPlayerLeft( ulong steamId ) {
+		SteamLobby.Instance.GetLobbyMembers();
+		
+		GetNode( "/root/Console" ).Call( "print_line", Players[ steamId ].MultiplayerUserName + " has faded away..." );
+		PlayerList.RemoveChild();
+		Players[ steamId ].QueueFree();
+		Players.Remove( steamId );
+	}
+	
 	public override void _Ready() {
 		Network = GetNode<Node2D>( "Network" );
 		SpawnTree = GetNode( "Network/Spawns" );
 		PauseMenu = GetNode<Control>( "CanvasLayer/PauseMenu" );
-		ClientHeartbeat = GetNode<Timer>( "ClientHeartbeatTimer" );
+		
 		ServerHeartbeat = GetNode<Timer>( "ServerHeartbeatTimer" );
 		ServerHeartbeat.Connect( "timeout", Callable.From( OnServerHeartbeatTimeout ) );
 
 		Players = new Dictionary<ulong, Player>();
 
 		PlayerScene = ResourceLoader.Load<PackedScene>( "res://scenes/Player.tscn" );
+		ThisPlayer = GetNode<Player>( "Network/Players/Player0" );
+		
+		PauseMenu = GetNode<Control>( "CanvasLayer/PauseMenu" );
+		PlayerList = GetNode<Node>( "Network/Players" );
+		
+		PauseMenu.Connect( "leave_lobby", Callable.From( SteamLobby.LeaveLobby ) );
+		SteamLobby.Instance.Connect( "PlayerJoined", Callable.From( OnPlayerJoined ) );
+		SteamLobby.Instance.Connect( "PlayerLeftLobby", Callable.From( OnPlayerLeft ) );
 	}
 };
