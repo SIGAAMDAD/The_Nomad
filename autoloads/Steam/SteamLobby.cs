5943ca7f81b61af4d9449cc68f2168073e4f103c
SIGAAMDAD <hankburne@gmail.com>
2025-03-25 15:31:17 -0500

    patching coop

--- autoloads/Steam/SteamLobby.cs
+++ autoloads/Steam/SteamLobby.cs
@@ -344,11 +344,11 @@ public partial class SteamLobby : Node {
 		if ( target == CSteamID.Nil ) {
 			for ( int i = 0; i < LobbyMembers.Count; i++ ) {
 				if ( LobbyMembers[i] != SteamManager.GetSteamID() ) {
-					SteamNetworking.SendP2PPacket( LobbyMembers[ i ], data, (uint)data.Length, EP2PSend.k_EP2PSendReliableWithBuffering, channel );
+					SteamNetworking.SendP2PPacket( LobbyMembers[ i ], data, (uint)data.Length, EP2PSend.k_EP2PSendReliable, channel );
 				}
 			}
 		} else {
-			SteamNetworking.SendP2PPacket( target, data, (uint)data.Length, EP2PSend.k_EP2PSendReliableWithBuffering, channel );
+			SteamNetworking.SendP2PPacket( target, data, (uint)data.Length, EP2PSend.k_EP2PSendReliable, channel );
 		}
 	}
 
