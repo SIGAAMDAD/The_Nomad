ac123bb423eb177c479f8841cef096963aea1d1e
Noah Van Til <86020846+SIGAAMDAD@users.noreply.github.com>
2025-04-01 08:28:45 -0500

    Update SteamLobby.cs

--- autoloads/Steam/SteamLobby.cs
+++ autoloads/Steam/SteamLobby.cs
@@ -9,25 +9,40 @@ public partial class SteamLobby : Node {
 		Public,
 		FriendsOnly
 	};
+	
+	public enum MessageType {
+		Handshake,
+		
+		// server commands
+		ServerCommand,
+
+		// hot cache player data
+		ClientData,
+
+		// world data, node paths that will never change
+		GameData,
+		
+		Count
+	};
 
 	private static SteamLobby _Instance;
 	public static SteamLobby Instance => _Instance;
-
+	
+	private const uint PACKET_READ_LIMIT = 32;
+	private const uint MAX_LOBBY_MEMBERS = 16;
+	
 	private Callback<LobbyEnter_t> LobbyEnter;
 	private Callback<LobbyChatMsg_t> LobbyChatMsg;
 	private Callback<LobbyChatUpdate_t> LobbyChatUpdate;
 	private Callback<P2PSessionRequest_t> P2PSessionRequest;
 	private Callback<P2PSessionConnectFail_t> P2PSessionConnectFail;
-	private Callback<SteamNetConnectionStatusChangedCallback_t> NetConnectionStatusChanged;
-	private Callback<SteamNetworkingMessagesSessionRequest_t> SteamNetworkingMessagesSessionRequest;
-	private Callback<SteamNetworkingMessagesSessionFailed_t> SteamNetworkingMessagesSessionFailed;
 
 	private CallResult<LobbyCreated_t> OnLobbyCreatedCallResult;
 	private CallResult<LobbyEnter_t> OnLobbyEnterCallResult;
 	private CallResult<LobbyMatchList_t> OnLobbyMatchListCallResult;
-
-	private Dictionary<CSteamID, SteamNetworkingIdentity> NetworkingSessions = new Dictionary<CSteamID, SteamNetworkingIdentity>();
-	public List<CSteamID> LobbyMembers = new List<CSteamID>();
+	
+	public CSteamID[] LobbyMembers = new CSteamID[ MAX_LOBBY_MEMBERS ];
+	public int LobbyMemberCount = 0;
 	private CSteamID LobbyId = CSteamID.Nil;
 	private CSteamID LobbyOwnerId = CSteamID.Nil;
 	private bool IsHost = false;
@@ -49,56 +64,12 @@ public partial class SteamLobby : Node {
 	private string LobbyFilterMap;
 	private string LobbyFilterGameMode;
 
-	private byte[] CachedPacket = new byte[ 1024 ];
+	private byte[] CachedPacket = null;
 	private System.IO.MemoryStream PacketStream = null;
 	private System.IO.BinaryReader PacketReader = null;
-
-	private const uint PACKET_READ_LIMIT = 32;
-
-	/*
-	public enum ChatRoomEnterResponse {
-		CHAT_ROOM_ENTER_RESPONSE_SUCCESS = 1,
-		CHAT_ROOM_ENTER_RESPONSE_DOESNT_EXIST,
-		CHAT_ROOM_ENTER_RESPONSE_NOT_ALLOWED,
-		CHAT_ROOM_ENTER_RESPONSE_FULL,
-		CHAT_ROOM_ENTER_RESPONSE_ERROR,
-		CHAT_ROOM_ENTER_RESPONSE_BANNED,
-		CHAT_ROOM_ENTER_RESPONSE_LIMITED,
-		CHAT_ROOM_ENTER_RESPONSE_CLAN_DISABLED,
-		CHAT_ROOM_ENTER_RESPONSE_COMMUNITY_BAN,
-		CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU,
-		CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER,
-		CHAT_ROOM_ENTER_RESPONSE_RATE_LIMIT_EXCEEDED
-	};
-
-	public enum ChatEntryType {
-		CHAT_ENTRY_TYPE_INVALID,
-		CHAT_ENTRY_TYPE_CHAT_MSG,
-		CHAT_ENTRY_TYPE_TYPING,
-		CHAT_ENTRY_TYPE_INVITE_GAME,
-		CHAT_ENTRY_TYPE_EMOTE,
-		CHAT_ENTRY_TYPE_LEFT_CONVERSATION,
-		CHAT_ENTRY_TYPE_ENTERED,
-		CHAT_ENTRY_TYPE_WAS_KICKED,
-		CHAT_ENTRY_TYPE_WAS_BANNED,
-		CHAT_ENTRY_TYPE_DISCONNECTED
-	};
-	*/
-
-	public enum MessageType {
-		Handshake,
-		
-		// lobby command: start game
-		StartGame,
-
-		// hot cache player data
-		ClientData,
-
-		// world data, node paths that will never change
-		GameData,
-		
-		Count
-	};
+	
+	private CSteamID ThisSteamID = CSteamID.Nil;
+	private int ThisSteamIDIndex = 0;
 
 	[Signal]
 	public delegate void ChatMessageReceivedEventHandler( ulong senderSteamId, string message );
@@ -180,37 +151,37 @@ public partial class SteamLobby : Node {
 	}
 
 	private void ChatUpdate( string status ) {
-		GetNode( "/root/Console" ).Call( "print_line", status, true );
+		Console.PrintLine( "status" );
 	}
 	private void OnLobbyChatUpdate( LobbyChatUpdate_t pCallback ) {
 		string changerName = SteamFriends.GetFriendPersonaName( (CSteamID)pCallback.m_ulSteamIDMakingChange );
 
 		switch ( (EChatMemberStateChange)pCallback.m_rgfChatMemberStateChange ) {
 		case EChatMemberStateChange.k_EChatMemberStateChangeEntered:
-			CallDeferred( "ChatUpdate", changerName + " has joined..." );
+			CallDeferred( "ChatUpdate", string.Format( "{0} has joined...", changerName ) );
 			CallDeferred( "emit_signal", "ClientJoinedLobby", pCallback.m_ulSteamIDMakingChange );
 			break;
 		case EChatMemberStateChange.k_EChatMemberStateChangeLeft:
-			CallDeferred( "ChatUpdate", changerName + " has faded away..." );
+			CallDeferred( "ChatUpdate", string.Format( "{0} has faded away...", changerName ) );
 			CallDeferred( "emit_signal", "ClientLeftLobby", pCallback.m_ulSteamIDMakingChange );
 			break;
 		case EChatMemberStateChange.k_EChatMemberStateChangeDisconnected:
-			CallDeferred( "ChatUpdate", changerName + " tweaked out so hard they left the fever dream..." );
+			CallDeferred( "ChatUpdate", string.Format( "{0} tweaked out so hard they left the fever dream...", changerName ) );
 			CallDeferred( "emit_signal", "ClientLeftLobby", pCallback.m_ulSteamIDMakingChange );
 			break;
 		case EChatMemberStateChange.k_EChatMemberStateChangeBanned:
-			CallDeferred( "ChatUpdate", changerName + " was rejected..." );
+			CallDeferred( "ChatUpdate", string.Format( "{0} was rejected...", changerName ) );
 			CallDeferred( "emit_signal", "ClientLeftLobby", pCallback.m_ulSteamIDMakingChange );
 			break;
 		case EChatMemberStateChange.k_EChatMemberStateChangeKicked:
-			CallDeferred( "ChatUpdate", changerName + " was excommunicated..." );
+			CallDeferred( "ChatUpdate", string.Format( "{0} was excommunicated...", changerName ) );
 			CallDeferred( "emit_signal", "ClientLeftLobby", pCallback.m_ulSteamIDMakingChange );
 			break;
 		};
 	}
 	private void OnLobbyCreated( LobbyCreated_t pCallback, bool bIOFailure ) {
 		if ( pCallback.m_eResult != EResult.k_EResultOK ) {
-			GD.PushError( "[STEAM] Error creating lobby!" );
+			Console.PrintLine( string.Format( "[STEAM] Error creating lobby: {0}", pCallback.m_eResult.ToString() ) );
 			return;
 		}
 
@@ -224,18 +195,19 @@ public partial class SteamLobby : Node {
 		SteamMatchmaking.SetLobbyData( LobbyId, "map", LobbyMap.ToString() );
 		SteamMatchmaking.SetLobbyData( LobbyId, "gamemode", LobbyGameMode.ToString() );
 		SteamMatchmaking.SetLobbyMemberLimit( LobbyId, LobbyMaxMembers );
+		
+		Console.PrintLine(
+			string.Format( "Created lobby [{0}] Name: {1}, MaxMembers: {2}, GameType: {3}",
+				LobbyId.ToString(), LobbyName, LobbyMaxMembers, GameConfiguration.GameMode.ToString()
+			)
+		);
 
-		GD.Print( "Created lobby [" + LobbyId.ToString() + "] Name: " + LobbyName + ", MaxMembers: " + LobbyMaxMembers + ", GameType: " + GameConfiguration.GameMode.ToString() );
-
-		bool bSetRelay = SteamNetworking.AllowP2PPacketRelay( true );
-		if ( !bSetRelay ) {
-			GD.PushError( "[STEAM] couldn't enable p2p packet relay!" );
+		if ( !SteamNetworking.AllowP2PPacketRelay( true ) ) {
+			Console.PrintError( "[STEAM] couldn't enable p2p packet relay!" );
 			return;
 		}
 	}
-	private void OnLobbyMatchList( LobbyMatchList_t pCallback, bool bIOFailure ) {	
-		GD.Print( "Received lobby match list." );
-
+	private void OnLobbyMatchList( LobbyMatchList_t pCallback, bool bIOFailure ) {
 		LobbyList.Clear();
 		for ( int i = 0; i < pCallback.m_nLobbiesMatching; i++ ) {
 			LobbyList.Add( SteamMatchmaking.GetLobbyByIndex( i ) );
@@ -244,30 +216,22 @@ public partial class SteamLobby : Node {
 		CallDeferred( "emit_signal", "LobbyListUpdated" );
 	}
 	private void MakeP2PHandkshake() {
-//		byte[] data = { (byte)MessageType.Handshake };
-//		SendP2PPacket( CSteamID.Nil, System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement( data, 0 ), data.Length );
-		SendP2PPacket( CSteamID.Nil, new byte[]{ (byte)MessageType.Handshake } );
+		SendP2PPacket( new byte[]{ (byte)MessageType.Handshake } );
 	}
 	public void GetLobbyMembers() {
-		LobbyMembers.Clear();
-
-		int memberCount = SteamMatchmaking.GetNumLobbyMembers( LobbyId );
-		for ( int i = 0; i < memberCount; i++ ) {
-			CSteamID steamId = SteamMatchmaking.GetLobbyMemberByIndex( LobbyId, i );
-			string username = SteamFriends.GetFriendPersonaName( steamId );
-
-			LobbyMembers.Add( steamId );
+		LobbyMemberCount = SteamMatchmaking.GetNumLobbyMembers( LobbyId );
+		for ( int i = 0; i < LobbyMembers.Length; i++ ) {
+			LobbyMembers[i] = CSteamID.Nil;
+		}
+		for ( int i = 0; i < LobbyMemberCount; i++ ) {
+			LobbyMembers[i] = SteamMatchmaking.GetLobbyMemberByIndex( LobbyId, i );
+			if ( LobbyMembers[i] == ThisSteamID ) {
+				ThisSteamIDIndex = i;
+			}
 		}
 	}
 
 	public void OpenLobbyList() {
-//		if ( LobbyFilterMap != "Any" ) {
-//			SteamMatchmaking.AddRequestLobbyListStringFilter( "map", LobbyFilterMap, ELobbyComparison.k_ELobbyComparisonEqual );
-//		}
-//		if ( LobbyFilterGameMode != "Any" ) {
-//			SteamMatchmaking.AddRequestLobbyListStringFilter( "gamemode", LobbyFilterGameMode, ELobbyComparison.k_ELobbyComparisonEqual );
-//		}
-
 		SteamMatchmaking.AddRequestLobbyListDistanceFilter( ELobbyDistanceFilter.k_ELobbyDistanceFilterWorldwide );
 
 		MatchMakingKeyValuePair_t[] filters = [];
@@ -296,7 +260,7 @@ public partial class SteamLobby : Node {
 			break;
 		};
 
-		GD.Print( "Initializing SteamLobby..." );
+		Console.PrintLine( "Initializing SteamLobby..." );
 		SteamAPICall_t handle = SteamMatchmaking.CreateLobby( lobbyType, LobbyMaxMembers );
 		OnLobbyCreatedCallResult.Set( handle );
 	}
@@ -312,7 +276,7 @@ public partial class SteamLobby : Node {
 		SteamMatchmaking.LeaveLobby( LobbyId );
 		LobbyId = CSteamID.Nil;
 
-		for ( int i = 0; i < LobbyMembers.Count; i++ ) {
+		for ( int i = 0; i < LobbyMemberCount; i++ ) {
 			P2PSessionState_t sessionState;
 			if ( !SteamNetworking.GetP2PSessionState( LobbyMembers[i], out sessionState ) ) {
 				continue;
@@ -321,7 +285,9 @@ public partial class SteamLobby : Node {
 				SteamNetworking.CloseP2PSessionWithUser( LobbyMembers[i] );
 			}
 		}
-		LobbyMembers.Clear();
+		for ( int i = 0; i < LobbyMembers.Length; i++ ) {
+			LobbyMembers[i] = CSteamID.Nil;
+		}
 
 		NodeCache.Clear();
 		PlayerCache.Clear();
@@ -346,17 +312,25 @@ public partial class SteamLobby : Node {
 		}
 	}
 	*/
+	public void SendTargetPacket( CSteamID target, byte[] data ) {
+		int channel = 0;
+		
+		SteamNetworking.SendP2PPacket( target, data, (uint)data.Length, EP2PSend.k_EP2PSendReliableWithBuffering, channel );
+	}
 	public void SendP2PPacket( CSteamID target, byte[] data ) {
 		int channel = 0;
-
-		if ( target == CSteamID.Nil ) {
-			for ( int i = 0; i < LobbyMembers.Count; i++ ) {
-				if ( LobbyMembers[i] != SteamManager.GetSteamID() ) {
-					SteamNetworking.SendP2PPacket( LobbyMembers[ i ], data, (uint)data.Length, EP2PSend.k_EP2PSendReliableWithBuffering, channel );
-				}
+		
+		for ( int i = 0; i < LobbyMemberCount; i += 2 ) {
+			if ( i != ThisSteamIDIndex ) {
+				SteamNetworking.SendP2PPacket( LobbyMembers[ i ], data, (uint)data.Length,
+					EP2PSend.k_EP2PSendReliableWithBuffering, channel
+				);
 			}
-		} else {
-			SteamNetworking.SendP2PPacket( target, data, (uint)data.Length, EP2PSend.k_EP2PSendReliableWithBuffering, channel );
+			if ( i + 1 != ThisSteamIDIndex ) {
+				SteamNetworking.SendP2PPacket( LobbyMembers[ i + 1 ], data, (uint)data.Length,
+					EP2PSend.k_EP2PSendReliableWithBuffering, channel
+				);
+			}
 		}
 	}
 
@@ -367,11 +341,6 @@ public partial class SteamLobby : Node {
 		}
 
 		CSteamID senderId;
-		/*
-		SteamNetworkingMessage_t message = System.Runtime.InteropServices.Marshal.PtrToStructure<SteamNetworkingMessage_t>( MessageBuffer[ packetIndex ] );
-		senderId = message.m_identityPeer.GetSteamID();
-		CachedPacket = System.Runtime.InteropServices.Marshal.PtrToStructure<byte[]>( message.m_pData );
-		*/
 		SteamNetworking.ReadP2PPacket( CachedPacket, packetSize, out packetSize, out senderId );
 		PacketStream.Seek( 0, System.IO.SeekOrigin.Begin );
 
@@ -379,8 +348,8 @@ public partial class SteamLobby : Node {
 		case MessageType.Handshake:
 			GD.Print( SteamFriends.GetFriendPersonaName( senderId ) + " sent a handshake packet." );
 			break;
-		case MessageType.StartGame:
-
+		case MessageType.ServerCommand:
+			ServerCommandManager.ExecuteCommand( (ServerCommandType)PacketReader.ReadUInt32() );
 			break;
 		case MessageType.ClientData:
 			PlayerCache[ senderId.ToString() ].Receive( PacketReader );
@@ -406,16 +375,21 @@ public partial class SteamLobby : Node {
 	}
 	
 	private void OnLobbyJoined( LobbyEnter_t pCallback, bool bIOFailure ) {
-		GD.Print( "...Joined" );
+		Console.PrintLine( "...Joined" );
 		if ( pCallback.m_EChatRoomEnterResponse != (uint)EChatRoomEnterResponse.k_EChatRoomEnterResponseSuccess ) {
-			GD.PushError( "[STEAM] Error joining lobby " + pCallback.m_ulSteamIDLobby + ": " + ( (EChatRoomEnterResponse)pCallback.m_EChatRoomEnterResponse ).ToString() );
+			Console.PrintError(
+				string.Format( "[STEAM] Error joining lobby {0}: {1}",
+					pCallback.m_ulSteamIDLobby,
+					( (EChatRoomEnterResponse)pCallback.m_EChatRoomEnterResponse ).ToString()
+				)
+			);
 			return;
 		}
 
 		LobbyId = (CSteamID)pCallback.m_ulSteamIDLobby;
 		LobbyOwnerId = SteamMatchmaking.GetLobbyOwner( LobbyId );
 
-		GD.Print( "Joined lobby " + pCallback.m_ulSteamIDLobby + "." );
+		Console.PrintLine( string.Format( "Joined lobby {0}.", pCallback.m_ulSteamIDLobby ) );
 
 		LobbyName = SteamMatchmaking.GetLobbyData( LobbyId, "name" );
 		switch ( SteamMatchmaking.GetLobbyData( LobbyId, "gametype" ) ) {
@@ -429,7 +403,9 @@ public partial class SteamLobby : Node {
 		LobbyMap = Convert.ToInt32( SteamMatchmaking.GetLobbyData( LobbyId, "map" ) );
 		LobbyGameMode = Convert.ToUInt32( SteamMatchmaking.GetLobbyData( LobbyId, "gamemode" ) );
 
+		// more for debugging...
 		GD.Print( "Sending p2p handshake..." );
+		
 		GetLobbyMembers();
 		MakeP2PHandkshake();
 		
@@ -437,16 +413,19 @@ public partial class SteamLobby : Node {
 	}
 
 	private void OnLobbyJoined( LobbyEnter_t pCallback ) {
-		GD.Print( "...Joined" );
 		if ( pCallback.m_EChatRoomEnterResponse != (uint)EChatRoomEnterResponse.k_EChatRoomEnterResponseSuccess ) {
-			GD.PushError( "[STEAM] Error joining lobby " + pCallback.m_ulSteamIDLobby + ": " + ( (EChatRoomEnterResponse)pCallback.m_EChatRoomEnterResponse ).ToString() );
+			Console.PrintError(
+				string.Format( "[STEAM] Error joining lobby {0}: {1}", pCallback.m_ulSteamIDLobby
+					( (EChatRoomEnterResponse)pCallback.m_EChatRoomEnterResponse ).ToString()
+				)
+			);
 			return;
 		}
 
 		LobbyId = (CSteamID)pCallback.m_ulSteamIDLobby;
 		LobbyOwnerId = SteamMatchmaking.GetLobbyOwner( LobbyId );
 
-		GD.Print( "Joined lobby " + pCallback.m_ulSteamIDLobby + "." );
+		Console.PrintLine( string.Format( "...Joined lobby {0}.", pCallback.m_ulSteamIDLobby ) );
 
 		LobbyName = SteamMatchmaking.GetLobbyData( LobbyId, "name" );
 		switch ( SteamMatchmaking.GetLobbyData( LobbyId, "gametype" ) ) {
@@ -500,7 +479,8 @@ public partial class SteamLobby : Node {
 		byte[] szBuffer = new byte[ 4096 ];
 		CSteamID senderId;
 		EChatEntryType entryType;
-		int ret = SteamMatchmaking.GetLobbyChatEntry( (CSteamID)pCallback.m_ulSteamIDLobby, (int)pCallback.m_iChatID, out senderId, szBuffer, szBuffer.Length, out entryType );
+		int ret = SteamMatchmaking.GetLobbyChatEntry( (CSteamID)pCallback.m_ulSteamIDLobby,
+			(int)pCallback.m_iChatID, out senderId, szBuffer, szBuffer.Length, out entryType );
 
 		switch ( entryType ) {
 		case EChatEntryType.k_EChatEntryTypeChatMsg: {
@@ -510,12 +490,12 @@ public partial class SteamLobby : Node {
 	}
 
 	private void OnRefreshComplete( HServerListRequest hRequest, EMatchMakingServerResponse response ) {
-		GD.Print( "[STEAM] Server list refresh finished: " + response.ToString() );
+		Console.PrintLine( string.Format( "[STEAM] Server list refresh finished: {0}", response.ToString() ) );
 	}
 	private void OnServerResponded( HServerListRequest hRequest, int iServer ) {
 	}
 	private void OnServerFailedToRespond( HServerListRequest hRequest, int iServer ) {
-		GD.PushError( "[STEAM] Server failed to respond" );
+		Console.PrintError( "[STEAM] Server failed to respond" );
 	}
 
 	public float GetPing() {
@@ -536,14 +516,13 @@ public partial class SteamLobby : Node {
 		LobbyChatUpdate = Callback<LobbyChatUpdate_t>.Create( OnLobbyChatUpdate );
 		P2PSessionRequest = Callback<P2PSessionRequest_t>.Create( OnP2PSessionRequest );
 
-//		SteamNetworkingMessagesSessionRequest = Callback<SteamNetworkingMessagesSessionRequest_t>.Create( OnSteamNetworkingMessagesSessionRequest );
-
 		OnLobbyCreatedCallResult = CallResult<LobbyCreated_t>.Create( OnLobbyCreated );
 		OnLobbyEnterCallResult = CallResult<LobbyEnter_t>.Create( OnLobbyJoined );
 		OnLobbyMatchListCallResult = CallResult<LobbyMatchList_t>.Create( OnLobbyMatchList );
 
 		ServerListResponse = new ISteamMatchmakingServerListResponse( OnServerResponded, OnServerFailedToRespond, OnRefreshComplete );
-
+		
+		CachedPacket = new byte[ 1024 ];
 		PacketStream = new System.IO.MemoryStream( CachedPacket );
 		PacketReader = new System.IO.BinaryReader( PacketStream );
 
@@ -553,6 +532,8 @@ public partial class SteamLobby : Node {
 		SetPhysicsProcess( false );
 		SetProcessInternal( false );
 		SetPhysicsProcessInternal( false );
+		
+		ThisSteamID = SteamManager.GetSteamID();
 	}
 	public override void _PhysicsProcess( double delta ) {
 		base._PhysicsProcess( delta );
