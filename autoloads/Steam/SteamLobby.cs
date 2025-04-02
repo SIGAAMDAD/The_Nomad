af45a5dd7715b956a5c55330e5b6d15e8b685006
SIGAAMDAD <hankburne@gmail.com>
2025-04-02 16:52:29 -0500

    fixing multiplayer

--- autoloads/Steam/SteamLobby.cs
+++ autoloads/Steam/SteamLobby.cs
@@ -431,11 +431,12 @@ public partial class SteamLobby : Node {
 			break;
 		case "Multiplayer":
 			GameConfiguration.GameMode = GameMode.Multiplayer;
+			LobbyMap = Convert.ToInt32( SteamMatchmaking.GetLobbyData( LobbyId, "map" ) );
+			LobbyGameMode = Convert.ToUInt32( SteamMatchmaking.GetLobbyData( LobbyId, "gamemode" ) );
+			Console.PrintLine( string.Format( "Lobby map: {0}", Convert.ToInt32( SteamMatchmaking.GetLobbyData( LobbyId, "map" ) ) ) );
 			break;
 		};
-		LobbyMap = Convert.ToInt32( SteamMatchmaking.GetLobbyData( LobbyId, "map" ) );
-		LobbyGameMode = Convert.ToUInt32( SteamMatchmaking.GetLobbyData( LobbyId, "gamemode" ) );
-
+		
 		GD.Print( "Sending p2p handshake..." );
 
 		GetLobbyMembers();
