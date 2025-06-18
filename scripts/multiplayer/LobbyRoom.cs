14135f927653d8cc337937ba924c9610a822095c
SIGAAMDAD <hankburne@gmail.com>
2025-06-18 15:12:11 -0500

    fixing some issues

--- scripts/multiplayer/LobbyRoom.cs
+++ scripts/multiplayer/LobbyRoom.cs
@@ -300,9 +300,6 @@ public partial class LobbyRoom : Control {
 		base._Process( delta );
 
 		for ( int i = 0; i < PlayerList.GetChildCount(); i++ ) {
-			if ( !SteamLobby.Instance.LobbyMembers[ i ].IsValid() ) {
-				continue;
-			}
 			HBoxContainer container = PlayerList.GetChild<HBoxContainer>( i );
 
 			{
