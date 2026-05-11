4f4f98c43caac0905f1be49714262f85fad831f0
SIGAAMDAD <hankburne@gmail.com>
2026-05-11 13:47:49 -0500

    integrating multiplayer profile statistics tracking, badges, and factional rewards dto/data structures

---
+++ Source/Game/Domain/Data/Multiplayer/Profile/ContractObjectiveType.cs
@@ -0,0 +1,34 @@
+/*
+===========================================================================
+The Nomad MPLv2 Source Code
+Copyright (C) 2025-2026 Noah Van Til
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v2. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This software is provided "as is", without warranty of any kind,
+express or implied, including but not limited to the warranties
+of merchantability, fitness for a particular purpose and noninfringement.
+===========================================================================
+*/
+
+namespace Nomad.Game.Domain.Data.Multiplayer.Profile
+{
+	public enum ContractObjectiveType : byte
+	{
+		WinMatches,
+		CompleteMatches,
+		WinDuels,
+		ClaimBounties,
+		CompleteExtractions,
+		DefendObjective,
+		CaptureFlags,
+		EarnParryKills,
+		EarnNoDeathRounds,
+		AssistFactionMembers,
+		UseFactionWeapon,
+		CompleteContractWithoutBulletTime,
+		CompleteContractWithLowSanity
+	};
+};
---
+++ Source/Game/Domain/Data/Multiplayer/Profile/ObjectiveStatsRecord.cs
@@ -0,0 +1,38 @@
+/*
+===========================================================================
+The Nomad MPLv2 Source Code
+Copyright (C) 2025-2026 Noah Van Til
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v2. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This software is provided "as is", without warranty of any kind,
+express or implied, including but not limited to the warranties
+of merchantability, fitness for a particular purpose and noninfringement.
+===========================================================================
+*/
+
+namespace Nomad.Game.Domain.Data.Multiplayer.Profile
+{
+	public sealed record ObjectiveStatsRecord
+	{
+		public uint ObjectivesCompleted { get; init; }
+		public uint ObjectivesDenied { get; init; }
+
+		public uint FlagsCaptured { get; init; }
+		public uint FlagsReturned { get; init; }
+
+		public uint ExtractionCompletions { get; init; }
+		public uint ExtractionDenials { get; init; }
+
+		public uint VipKills { get; init; }
+		public uint VipSaves { get; init; }
+
+		public uint BountiesClaimed { get; init; }
+		public uint BountyEscapes { get; init; }
+
+		public uint HoldLineSeconds { get; init; }
+		public uint AlliesRevived { get; init; }
+	};
+};
